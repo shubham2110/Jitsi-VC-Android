f18b181c420a7d13aacc3e1663e009cdbf3ece1b
unknown <yashbnit@gmail.com>
2020-04-27 13:28:36 +0530

    First Commit

---
+++ app/src/main/java/com/example/ioc_vc_connect/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.ioc_vc_connect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.conferenceName);
+
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    RelativeLayout container = findViewById(R.id.container1);
+                    container.setVisibility(View.VISIBLE);
+                } else {
+                    RelativeLayout container = findViewById(R.id.container1);
+                    container.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        RelativeLayout container = findViewById(R.id.top_container);
+        container.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    editText.clearFocus();
+                }
+            }
+        });
+
+        // Initialize default options for Jitsi Meet conferences.
+        URL serverURL;
+        try {
+            serverURL = new URL("https://ioc.instantconnect.in");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Invalid server URL!");
+        }
+        JitsiMeetConferenceOptions defaultOptions
+                = new JitsiMeetConferenceOptions.Builder()
+                .setServerURL(serverURL)
+                .setWelcomePageEnabled(false)
+                .build();
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+    }
+
+    public void onButtonClick(View v) {
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+
+        if (text.length() > 0) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            JitsiMeetConferenceOptions options
+                    = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .build();
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(this, options);
+        }
+    }
+}
